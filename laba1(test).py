f5c44547e9a3a66240255b691db217b13be098f3
Bogdan Havran <danohavran@gmail.com>
2019-11-14 19:06:51 +0200

    Add files via upload

---
+++ laba1(test).py
@@ -0,0 +1,17 @@
+alphabet = "abcdefghijklmnopqrstuvwxyzaабвгґдеєжзиіїйклмнопрстуфхцчшщьюяа12345678901234567890"
+
+while True:
+    encrypt = input("Enter text: ")
+    key = 1
+    encrypt = encrypt.lower()
+    encrypted = ""
+
+
+    for letter in encrypt:
+        position = alphabet.find(letter)
+        newPosition = position + key
+        if letter in alphabet:
+            encrypted = encrypted + alphabet[newPosition]
+        else:
+            encrypted = encrypted +letter
+    print("Your cipher is: ",encrypted)
